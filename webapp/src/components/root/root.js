a0dc4a85068a7a8d44d3bc381c8d8dfcca3d0c6e
Jerry <calljerryli@outlook.com>
2020-08-28 15:38:20 +0800

    chore update package.json

--- webapp/src/components/root/root.js
+++ webapp/src/components/root/root.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {CLOSE} from '../../action_types';
 import Axios from 'axios';
